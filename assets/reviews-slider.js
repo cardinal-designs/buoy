c763dadae6acd72f9f7f6a001c8ba8bd74c13a69
nataliedespain <despain.natalie@gmail.com>
2024-08-09 09:51:26 -0600

    chronic illness progress

--- assets/reviews-slider.js
+++ assets/reviews-slider.js
@@ -1,43 +1,29 @@
-$(function(){
-  function slickOnResize() {
-    $(".Reviews_Slider_Container").not('.slick-initialized').slick({
-      lazyLoad: 'progressive',
-      dots: true,
-      arrows: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="29.013" height="12.007" viewBox="0 0 29.013 12.007"><g id="Arrow" transform="translate(272.182 1503.504) rotate(180)"><path id="Path_3" data-name="Path 3" d="M4.98,11,0,5.5,4.98,0" transform="translate(271.17 1503) rotate(180)" fill="none" stroke="#1f2322" stroke-width="1.5"></path><path id="Path_4" data-name="Path 4" d="M249.357,1495.977h28" transform="translate(-6.188 1.523)" fill="none" stroke="#1f2322" stroke-width="1.5"></path></g></svg></button>',
-      nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="29.013" height="12.007" viewBox="0 0 29.013 12.007"><g id="Arrow" transform="translate(28.001 11.504) rotate(180)"><g id="Arrow-2" data-name="Arrow" transform="translate(271.17 1503) rotate(180)"><path id="Path_3" data-name="Path 3" d="M4.98,11,0,5.5,4.98,0" transform="translate(271.17 1503) rotate(180)" fill="none" stroke="#1f2322" stroke-width="1.5"></path><path id="Path_4" data-name="Path 4" d="M249.357,1495.977h28" transform="translate(-6.188 1.523)" fill="none" stroke="#1f2322" stroke-width="1.5"></path></g></g></svg></button>',
-      responsive: [
-        {
-          breakpoint: 768,
-          settings : {
-            arrows: false,
-          }
+$(window).on('load', function (){
+  $('.reviews-slider__slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    gap: 24,
+    prevArrow: '<button type="button" class="slick-prev"><svg width="24" height="21" viewBox="0 0 24 21" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="2.19116" y1="10.5566" x2="23.7245" y2="10.5566" stroke="#1F2322"/><path d="M10.9489 20L1.44886 10.5L10.9489 1.00002" stroke="#1F2322"/></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg width="24" height="21" viewBox="0 0 24 21" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="22.2577" y1="10.4434" x2="0.724379" y2="10.4434" stroke="#1F2322"/><path d="M13.5 1L23 10.5L13.5 20" stroke="#1F2322"/></svg></button>',
+    appendArrows: '.reviews-slider__arrows',
+    responsive: [
+      {
+        breakpoint: 1125,
+        settings : {
+          slidesToShow: 2,
         }
-      ]
-    });
-  }
-  if (window.innerWidth < 768) {
-    slickOnResize();
-  }
-  $(window).on('load resize',function(){
-    var $window_width = window.innerWidth;
-    console.log(window.innerWidth);
-    if ($window_width < 768) {
-      if($(".Reviews_Slider_Container").hasClass('slick-initialized')){
-        $(".Reviews_Slider_Container").slick("resize");
+      },
+      {
+        breakpoint: 768,
+        settings : {
+          arrows: false,
+          slidesToShow: 1,
+        }
       }
-      else{
-        slickOnResize();
-      }
-    }
-    else{
-      if($(".Reviews_Slider_Container").hasClass('slick-initialized')){
-        $(".Reviews_Slider_Container").slick('unslick');
-      }
-    }
+    ]
   });
 });
